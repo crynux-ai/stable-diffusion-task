243ac81f5df45355a1284bbae85c9a6dde71fe58
iwehf <henry.lee@crynux.ai>
2024-03-28 19:40:14 +0800

    support run in windows

--- sd_task/utils.py
+++ sd_task/utils.py
@@ -1,26 +1,21 @@
-import enum
 import platform
-from typing import Union
 
-class Platform(enum.Enum):
-    UNKNONW = 0
-    LINUX_CUDA = 1
-    MACOS_MPS = 2
-
-def get_platform() -> Union[Platform, str]:
-    res = platform.system()
-    if res == "Linux":
-        return Platform.LINUX_CUDA
-    elif res == "Darwin":
-        return Platform.MACOS_MPS
-    else:
-        return res
 
 def get_accelerator():
-    platform = get_platform()    
-    if platform == Platform.LINUX_CUDA:
-        return "cuda"
-    elif platform == Platform.MACOS_MPS:
-        return "mps"
-    else:
-        return "cpu"
+    if platform.system() == "Darwin":
+        try:
+            import torch.mps
+
+            return "mps"
+        except ImportError:
+            pass
+
+    try:
+        import torch.cuda
+
+        if torch.cuda.is_available():
+            return "cuda"
+    except ImportError:
+        pass
+
+    return "cpu"
