7f1f7992a006b153b58a43e9f38c8ab130c42485
iwehf <henry.lee@crynux.ai>
2024-08-09 21:29:08 +0800

    add sd finetune task

---
+++ sd_task/finetune_task_args/__init__.py
@@ -0,0 +1,16 @@
+from .lora_task_args import (AdamOptimizerArgs, DatasetArgs,
+                             FinetuneLoraTaskArgs, LoraArgs, LRSchedulerArgs,
+                             ModelArgs, TrainArgs, TransformArgs,
+                             ValidationArgs)
+
+__all__ = [
+    "AdamOptimizerArgs",
+    "DatasetArgs",
+    "FinetuneLoraTaskArgs",
+    "LoraArgs",
+    "LRSchedulerArgs",
+    "ModelArgs",
+    "TrainArgs",
+    "TransformArgs",
+    "ValidationArgs",
+]
