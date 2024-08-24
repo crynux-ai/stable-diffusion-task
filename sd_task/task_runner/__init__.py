acaaa7b92905557eaf32fd52928d7a4889e87303
iwehf <henry.lee@crynux.ai>
2024-08-24 21:49:55 +0800

    export run_controlnet_preprocess_task in task_runner

--- sd_task/task_runner/__init__.py
+++ sd_task/task_runner/__init__.py
@@ -1,3 +1,4 @@
+from .controlnet_preprocess import run_controlnet_preprocess_task
 from .finetune_task import run_finetune_lora_task
 from .inference_task import run_inference_task
 from .remove_background import run_remove_background_task
@@ -8,4 +9,5 @@ __all__ = [
     "run_finetune_lora_task",
     "run_remove_background_task",
     "run_tagger_task",
+    "run_controlnet_preprocess_task",
 ]
